e5c55ca0ae3ab51a5c2a617499db74e65f4aeb19
Jack Lansing <jack.lansing@live.com>
2020-05-03 23:36:11 -0400

    hanoi

---
+++ hanoi.js
@@ -0,0 +1,19 @@
+function hanoi(n, src, dest, temp) {
+  if (n === 1) {
+    print(src, dest);
+  }
+
+  if (n > 1) {
+    hanoi(n - 1, src, temp, dest);
+    print(src, dest);
+    hanoi(n - 1, temp, dest, src);
+  }
+}
+
+function print(a, b) {
+  console.log(`Moving ${a} to -> ${b}`);
+}
+
+// hanoi(2, 'A', 'C', 'B');
+hanoi(3, 'A', 'C', 'B');
+// hanoi(5, 'A', 'C', 'B');
